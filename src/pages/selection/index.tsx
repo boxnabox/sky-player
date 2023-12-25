c01beafdef205302cc575a767d6d80fc7d8ea610
boxnabox <topinamburkopai@gmail.com>
2023-12-25 13:42:54 +0300

    structure
    blank pages added

--- src/pages/selection/index.tsx
+++ src/pages/selection/index.tsx
@@ -3,7 +3,7 @@ import * as S from './index.style';
 export default function Login() {
   return (
     <S.Container>
-      <div className="login">LOGIN</div>
+      <div className="login">SELECTION</div>
     </S.Container>
   );
 }
